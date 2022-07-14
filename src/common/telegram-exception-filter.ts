3d1e3cac109f67b82ee3bb14085d3819a629fffb
Konstantin Kosachyov <sidgrouse@gmail.com>
2022-07-14 23:14:18 +0300

    google api error message fix

--- src/common/telegram-exception-filter.ts
+++ src/common/telegram-exception-filter.ts
@@ -5,9 +5,13 @@ import { Context } from 'telegraf';
 @Catch()
 export class TelegrafExceptionFilter implements ExceptionFilter {
   async catch(exception: Error, host: ArgumentsHost): Promise<void> {
+    let message = `<b>Error</b>: ${exception.message}`;
+    if (exception.message.startsWith('Google API error')){
+      message = 'Гугл устал, подождите минутку';
+    }
     const telegrafHost = TelegrafArgumentsHost.create(host);
     const ctx = telegrafHost.getContext<Context>();
 
-    await ctx.replyWithHTML(`<b>Error</b>: ${exception.message}`);
+    await ctx.replyWithHTML(message);
   }
 }
